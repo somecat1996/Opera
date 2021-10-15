cf1555e81820fd30db5f59ace16f86fe1938779e
Lynn Wang <369150573@qq.com>
2021-10-15 16:55:02 +0800

    飞刀毒云

--- Assets/Scripts/SummonObjects/PoisonCloud.cs
+++ Assets/Scripts/SummonObjects/PoisonCloud.cs
@@ -6,6 +6,7 @@ public class PoisonCloud : MonoBehaviour
 {
     public float poisonTime = 1f;
 
+    private List<GameObjectInterface> enemies;
     private float poisonTimer;
     private float life;
     // Start is called before the first frame update
@@ -13,6 +14,7 @@ public class PoisonCloud : MonoBehaviour
     {
         life = Mathf.Infinity;
         poisonTimer = poisonTime;
+        enemies = new List<GameObjectInterface>();
     }
 
     // Update is called once per frame
@@ -25,7 +27,8 @@ public class PoisonCloud : MonoBehaviour
         if (poisonTimer <= 0)
         {
             poisonTimer = poisonTime;
-
+            foreach (GameObjectInterface tmp in enemies)
+                tmp.Poisoning();
         }
     }
 
@@ -33,5 +36,21 @@ public class PoisonCloud : MonoBehaviour
     {
         life = time;
         transform.localScale *= range;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            enemies.Add(other.GetComponent<GameObjectInterface>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            enemies.Remove(other.GetComponent<GameObjectInterface>());
+        }
     }
 }
