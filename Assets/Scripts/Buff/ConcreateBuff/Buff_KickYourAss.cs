f57bb730e150c07b3916d5acbb56b997f7b43009
BlankFactor <787121798@qq.com>
2021-10-19 16:54:14 +0800

    修改部分设置 等待补完Buff303的功能

--- Assets/Scripts/Buff/ConcreateBuff/Buff_KickYourAss.cs
+++ Assets/Scripts/Buff/ConcreateBuff/Buff_KickYourAss.cs
@@ -7,6 +7,8 @@ public class Buff_KickYourAss : BuffPrototype
     public float duration = 10f;
     public float probability = 0.08f;
 
+    public GameObjectBase lastEnemy = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,36 @@ public class Buff_KickYourAss : BuffPrototype
     // Update is called once per frame
     void Update()
     {
-        
+        if(lastEnemy != BattleDataManager.instance.lastTargetEnemy)
+        {
+            lastEnemy = BattleDataManager.instance.lastTargetEnemy;
+
+            if(Random.Range(0,1f) < GetTrueMainValue())
+            {
+                // ²¹³äÕÙ»½Î×¶¾ÍÞÍÞµÄ´úÂë
+
+            }
+        }
     }
 
     public override float GetTrueMainValue()
     {
         return GlobalValue.GetTrueProbaility(probability);
+
+    }
+
+    private void OnEnable()
+    {
+        lastEnemy = BattleDataManager.instance.lastTargetEnemy;
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        OnDisable();
     }
 }
