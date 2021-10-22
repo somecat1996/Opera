110ba196e463b8e1ebb36ee9a6c4308f9838eb98
Lynn Wang <369150573@qq.com>
2021-10-22 21:28:56 +0800

    Update EffectsManager.cs

--- Assets/Scripts/Global/EffectsManager.cs
+++ Assets/Scripts/Global/EffectsManager.cs
@@ -19,18 +19,39 @@ public class EffectsManager : MonoBehaviour
             Test();
     }
 
+
+    /// <summary>
+    ///  创建一个固定的特效
+    /// </summary>
+    /// <param name="id">特效ID</param>
+    /// <param name="_life">生存时间</param>
+    /// <param name="position">位置</param>
+    /// <param name="offset">对位置的偏移</param>
     public void CreateEffect(int id, float _life, Vector3 position, Vector3 offset)
     {
         GameObject tmp = Instantiate(effects[id]);
         tmp.GetComponent<EffectBase>().Instantiate(_life, position, offset);
     }
 
+    /// <summary>
+    ///  创建一个跟随目标的特效
+    /// </summary>
+    /// <param name="id">特效ID</param>
+    /// <param name="_life">生存时间</param>
+    /// <param name="_target">目标transform</param>
+    /// <param name="offset">对位置的偏移</param>
     public void CreateEffectFollow(int id, float _life, Transform _target, Vector3 _offset)
     {
         GameObject tmp = Instantiate(effects[id]);
         tmp.GetComponent<EffectFollow>().InstantiateFollow(_life, _target, _offset);
     }
 
+    /// <summary>
+    ///  创建一个跟随玩家的特效
+    /// </summary>
+    /// <param name="id">特效ID</param>
+    /// <param name="_life">生存时间</param>
+    /// <param name="offset">对位置的偏移</param>
     public void CreateEffectFollowPlayer(int id, float _life, Vector3 _offset)
     {
         GameObject tmp = Instantiate(effects[id]);
